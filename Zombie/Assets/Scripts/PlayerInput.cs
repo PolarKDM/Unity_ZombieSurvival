8843cd68abc8992b8a2a797bebded72298624807
PolarKDM <ehdals3408@naver.com>
2021-12-26 21:56:15 +0900

    1.1
    player 구현, 메인 카메라 설정

--- Zombie/Assets/Scripts/PlayerInput.cs
+++ Zombie/Assets/Scripts/PlayerInput.cs
@@ -3,12 +3,13 @@
 // 플레이어 캐릭터를 조작하기 위한 사용자 입력을 감지
 // 감지된 입력값을 다른 컴포넌트들이 사용할 수 있도록 제공
 public class PlayerInput : MonoBehaviour {
+    // Unity Input에 할당한 입력 축, 버튼은 Input Manager에서 확인가능.
     public string moveAxisName = "Vertical"; // 앞뒤 움직임을 위한 입력축 이름
     public string rotateAxisName = "Horizontal"; // 좌우 회전을 위한 입력축 이름
     public string fireButtonName = "Fire1"; // 발사를 위한 입력 버튼 이름
     public string reloadButtonName = "Reload"; // 재장전을 위한 입력 버튼 이름
 
-    // 값 할당은 내부에서만 가능
+    // property 값 할당은 내부에서만 가능
     public float move { get; private set; } // 감지된 움직임 입력값
     public float rotate { get; private set; } // 감지된 회전 입력값
     public bool fire { get; private set; } // 감지된 발사 입력값
@@ -26,13 +27,14 @@ public class PlayerInput : MonoBehaviour {
             return;
         }
 
+        
         // move에 관한 입력 감지
-        move = Input.GetAxis(moveAxisName);
+        move = Input.GetAxis(moveAxisName); // GetAxis(string) : 입력으로 감지할 축 이름을 받아 감지된 입력을 숫자로 반환
         // rotate에 관한 입력 감지
         rotate = Input.GetAxis(rotateAxisName);
         // fire에 관한 입력 감지
-        fire = Input.GetButton(fireButtonName);
+        fire = Input.GetButton(fireButtonName); // GetButton(string) : 입력으로 감지할 버튼 이름을 받아 해당 버튼을 누르고 있는 동안엔 true 아니면 false
         // reload에 관한 입력 감지
-        reload = Input.GetButtonDown(reloadButtonName);
+        reload = Input.GetButtonDown(reloadButtonName); // GetButtonDown(string) : 버튼을 누르기 시작한 순간만 true
     }
 }
--- Zombie/Assets/Scripts/PlayerMovement.cs
+++ Zombie/Assets/Scripts/PlayerMovement.cs
@@ -12,20 +12,40 @@ public class PlayerMovement : MonoBehaviour {
 
     private void Start() {
         // 사용할 컴포넌트들의 참조를 가져오기
+        playerInput = GetComponent<PlayerInput>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
     private void FixedUpdate() {
         // 물리 갱신 주기마다 움직임, 회전, 애니메이션 처리 실행
+        Rotate();
+        Move();
+
+        // 입력값에 따라 애니메이터의 Move 파라미터값 변경
+        playerAnimator.SetFloat("Move", playerInput.move);
     }
 
     // 입력값에 따라 캐릭터를 앞뒤로 움직임
     private void Move() {
+        // 상대적으로 이동할 거리 계산 (사용자입력값 * 방향 * 속력 * 시간)
+        Vector3 moveDistance = playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
+        // 리지드바디를 이용해 게임 오브젝트 위치 변경.
+        // MovePosition(Vector3) : 해당 좌표(전역좌표)로 이동
+        // 전역좌표 = playerRigidbody.position(현재좌표) + moveDistance(상대좌표)
+        playerRigidbody.MovePosition(playerRigidbody.position + moveDistance);
 
+        // transform 컴포넌트를 사용하지않은 이유 : 벽에 닿을시 충돌을 무시함.
+        // Rigidbody.MovePosition() : 위치값을 변경하면 이동경로에 다른 콜라이더 존재시 밀어내거나 밀려나는 물리처리 실행
     }
 
     // 입력값에 따라 캐릭터를 좌우로 회전
     private void Rotate() {
-
+        // 상대적으로 회전할 수치 계산 (사용자입력값 * 회전속도 * 시간)
+        float turn = playerInput.rotate * rotateSpeed * Time.deltaTime;
+        // 리지드바디를 이용해 게임 오브젝트 회전 변경
+        // 어떤 회전상태에서 상대적으로 더 회전 -> Quaternion 사용 (현재회전상태 * (0, turn, 0f)만큼 더 회전)
+        playerRigidbody.rotation = playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
     }
 }
